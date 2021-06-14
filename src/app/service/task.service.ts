171da1adab6e4739224cb8be67a08b6e7a1a4458
Leo Sinani <kajayr_sinanian@yahoo.com>
2021-06-13 17:41:32 -0700

    finilized bootstrap added ready to use

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,6 +1,6 @@
+import { Task } from './../Task';
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
-import {Task} from '../Task';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 const httpOptions = {
@@ -26,5 +26,8 @@ return this.http.delete<Task>(url);
   updateTaskReminder(task:Task):Observable<Task>{
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.put<Task>(url, task, httpOptions)
+  }
+  addTask(task:Task):Observable<Task>{
+    return this.http.post<Task>(this.apiUrl, task, httpOptions)
   }
 }
